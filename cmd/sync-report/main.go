7a7a683d6e79a4ff64a85bce4675c629088509ca
Rikard Legge <rikard.legge@gmail.com>
2019-09-21 05:54:19 +0200

    Add documentation and logging

--- cmd/sync-report/main.go
+++ cmd/sync-report/main.go
@@ -3,35 +3,87 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"izettle-daily-reports/google"
 	"izettle-daily-reports/izettle"
+	"izettle-daily-reports/storage"
 	"log"
 	"os"
-	"regexp"
-	"strings"
-	"time"
 
 	"github.com/joho/godotenv"
 	"google.golang.org/api/drive/v3"
 	"google.golang.org/api/option"
 )
 
-func getTargetFolders(ds *drive.Service, dir string) (map[string]*drive.File, error) {
-	folderList, err := ds.Files.List().
-		Q(fmt.Sprintf("'%s' in parents", dir)).
-		IncludeItemsFromAllDrives(true).
-		SupportsTeamDrives(true).
-		Do()
+func main() {
+	fmt.Print("Loading .env file... ")
+	err := godotenv.Load()
 	if err != nil {
-		return nil, err
+		fmt.Printf("FAILED: %s\n", err)
+	} else {
+		fmt.Println("DONE")
 	}
 
-	folders := make(map[string]*drive.File)
-	for _, folder := range folderList.Files {
-		folders[folder.Name] = folder
+	fmt.Print("Reading environment variables... ")
+	applicationID := MustGetEnv("CLIENT_ID")
+	applicationSecret := MustGetEnv("CLIENT_SECRET")
+	driveDirectoryID := MustGetEnv("GDRIVE_FOLDER_ID")
+	izettleEmail := MustGetEnv("IZETTLE_EMAIL")
+	izettlePassword := MustGetEnv("IZETTLE_PASSWORD")
+	fmt.Println("DONE")
+
+	fmt.Print("Logging in to your google account... ")
+	token, err := google.Login(applicationID, applicationSecret, []string{drive.DriveScope})
+	handleError(err)
+	driveService, err := drive.NewService(context.Background(), option.WithTokenSource(*token))
+	handleError(err)
+	folders, err := storage.Children(driveService, driveDirectoryID)
+	handleError(err)
+	fmt.Println("DONE")
+
+	fmt.Print("Logging in to your izettle account... ")
+	izettleClient, err := izettle.Login(izettleEmail, izettlePassword)
+	handleError(err)
+	users, err := izettleClient.ListUsers()
+	handleError(err)
+	fmt.Println("DONE")
+
+	fmt.Println("Looking for missing PDFs...")
+	missingPDFs := make([]storage.MissingPDF, 0)
+	for _, user := range users {
+		fmt.Printf("Fetching reports for %s...\n", user.Name)
+		folder := folders[user.Name]
+		reports, err := izettleClient.ListReports(user)
+		handleError(err)
+		fmt.Printf(" - Comparing %d report(s) against Google Drive...\n", len(reports))
+		missing, err := storage.MissingPDFs(driveService, folder, reports)
+		handleError(err)
+		fmt.Printf(" - Found %d missing report(s)\n", len(missing))
+		for _, r := range missing {
+			fmt.Printf("    * %s\n", r.Report.From)
+			missingPDFs = append(missingPDFs, r)
+		}
 	}
-	return folders, nil
+	fmt.Println()
+
+	if len(missingPDFs) == 0 {
+		fmt.Println("No files seem to be missing so we are done!")
+		os.Exit(0)
+	}
+
+	fmt.Printf("Creating %d missing PDF(s)...\n", len(missingPDFs))
+	for i, r := range missingPDFs {
+		filePath := fmt.Sprintf("%s/%s/%s", r.Report.User.Name, r.Report.From, storage.ReportFileName(r.Report))
+		fmt.Printf(" - Processing report %d of %d (%s)", i, len(missingPDFs), filePath)
+		fmt.Printf("    * Generating...")
+		pdf, err := izettleClient.ReportToPDF(r.Report)
+		handleError(err)
+		fmt.Printf("    * Uploading...")
+		err = storage.UploadPDF(driveService, r, pdf)
+		handleError(err)
+	}
+	fmt.Println()
+
+	fmt.Println("Finished generating reports!")
 }
 
 func handleError(err error) {
@@ -40,158 +92,10 @@ func handleError(err error) {
 	}
 }
 
-type ReportFile struct {
-	report izettle.Report
-	dir    *drive.File
-}
-
-func getToday() string {
-	year, month, day := time.Now().Date()
-	return fmt.Sprintf("%d-%d-%d", year, month, day)
-}
-func missingReports(ds *drive.Service, root *drive.File, reports []izettle.Report) ([]ReportFile, error) {
-	missing := make([]ReportFile, 0)
-	today := getToday()
-
-	reportYears := make(map[string][]izettle.Report)
-	for _, report := range reports {
-		year := strings.Split(report.From, "-")[0]
-		reportYears[year] = append(reportYears[year], report)
+func MustGetEnv(name string) string {
+	value := os.Getenv(name)
+	if value == "" {
+		log.Fatalf("Failed to get environment variable '%s'", value)
 	}
-	yearDirList, err := ds.Files.List().
-		Q(fmt.Sprintf("'%s' in parents", root.Id)).
-		IncludeItemsFromAllDrives(true).
-		SupportsTeamDrives(true).
-		Do()
-	if err != nil {
-		return nil, err
-	}
-	dirYears := make(map[string]*drive.File)
-	for _, file := range yearDirList.Files {
-		dirYears[file.Name] = file
-	}
-
-	for year, reports := range reportYears {
-		yearDir, ok := dirYears[year]
-		if !ok {
-			for _, report := range reports {
-				if report.From == today {
-					continue
-				}
-				missing = append(missing, ReportFile{report: report, dir: root})
-			}
-			continue
-		}
-		files, err := ds.Files.List().
-			Q(fmt.Sprintf("'%s' in parents", yearDir.Id)).
-			IncludeItemsFromAllDrives(true).
-			SupportsTeamDrives(true).
-			Do()
-		if err != nil {
-			return nil, err
-		}
-		filesByDate := make(map[string]*drive.File)
-		for _, file := range files.Files {
-			date, err := fileDate(file.Name)
-			if err != nil {
-				continue
-			}
-			filesByDate[date] = file
-		}
-		for _, report := range reports {
-			if _, ok := filesByDate[report.From]; !ok {
-				if report.From == today {
-					continue
-				}
-				missing = append(missing, ReportFile{report: report, dir: yearDir})
-			}
-		}
-	}
-
-	return missing, nil
-}
-
-func uploadPDF(ds *drive.Service, report ReportFile, data io.Reader) error {
-	isYear := regexp.MustCompile("\\d+")
-	dir := report.dir
-	if !isYear.Match([]byte(dir.Name)) {
-		year := strings.Split(report.report.From, "-")[0]
-		dir = &drive.File{
-			MimeType: "application/vnd.google-apps.folder",
-			Name:     year,
-			Parents:  []string{report.dir.Id},
-		}
-		_, err := ds.Files.
-			Create(dir).
-			SupportsTeamDrives(true).
-			Do()
-		if err != nil {
-			return err
-		}
-	}
-	f := &drive.File{
-		MimeType: "application/pdf",
-		Name:     reportName(report.report),
-		Parents:  []string{report.dir.Id},
-	}
-	_, err := ds.Files.
-		Create(f).
-		SupportsTeamDrives(true).
-		Media(data).
-		Do()
-	return err
-}
-
-func reportName(report izettle.Report) string {
-	return fmt.Sprintf("%s_%s.pdf", report.User.Name, report.From)
-}
-
-func fileDate(name string) (string, error) {
-	getDate := regexp.MustCompile(".*_(\\d+-\\d+-\\d+).pdf")
-	date := getDate.FindStringSubmatch(name)
-	if date == nil {
-		return "", fmt.Errorf("failed to get date from file")
-	}
-	return date[1], nil
-}
-
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Printf("Failed to load .env file: %s", err)
-	}
-	clientID := os.Getenv("CLIENT_ID")
-	clientSecret := os.Getenv("CLIENT_SECRET")
-	token, err := google.Login(clientID, clientSecret, []string{drive.DriveScope})
-	handleError(err)
-	driveService, err := drive.NewService(context.Background(), option.WithTokenSource(*token))
-	handleError(err)
-	dir := os.Getenv("GDRIVE_FOLDER_ID")
-	folders, err := getTargetFolders(driveService, dir)
-	handleError(err)
-
-	user := os.Getenv("IZETTLE_EMAIL")
-	password := os.Getenv("IZETTLE_PASSWORD")
-	izettle, err := izettle.Login(user, password)
-	handleError(err)
-	users, err := izettle.ListUsers()
-	handleError(err)
-
-	toGenerate := make([]ReportFile, 0)
-	for komitee, user := range users {
-		folder := folders[komitee]
-		reports, err := izettle.ListReports(user)
-		handleError(err)
-		missing, err := missingReports(driveService, folder, reports)
-		handleError(err)
-		for _, r := range missing {
-			toGenerate = append(toGenerate, r)
-		}
-	}
-	for _, r := range toGenerate {
-		pdf, err := izettle.GenerateReport(r.report)
-		handleError(err)
-		err = uploadPDF(driveService, r, pdf)
-		handleError(err)
-	}
+	return value
 }
--- izettle/izettle.go
+++ izettle/izettle.go
@@ -20,13 +20,14 @@ type Client struct {
 }
 
 func Login(user, password string) (*Client, error) {
+	// Create cookie jar to store cookies which are set by later requests
 	jar, err := cookiejar.New(nil)
 	if err != nil {
 		return nil, err
 	}
-	client := &http.Client{
-		Jar: jar,
-	}
+	client := &http.Client{Jar: jar}
+
+	// Get csrf token from login page
 	loginURL := "https://login.izettle.com/login?username=" + url.QueryEscape(user)
 	resp, err := client.Get(loginURL)
 	if err != nil {
@@ -37,28 +38,28 @@ func Login(user, password string) (*Client, error) {
 	if err != nil {
 		return nil, err
 	}
-	// Find the review items
 	n := doc.Find("[name=_csrf]").First()
 	token, _ := n.Attr("value")
 
+	// Create the login form and simulate a login
 	form := url.Values{}
 	form.Add("_csrf", token)
 	form.Add("username", user)
 	form.Add("password", password)
 	form.Add("button", "")
-
 	resp, err = client.PostForm(loginURL, form)
 	if err != nil {
 		return nil, err
 	}
 	_ = resp.Body.Close()
+
+	// Make sue that the login added new cookies to the root domain
 	host, _ := url.Parse("https://izettle.com")
 	if len(jar.Cookies(host)) == 0 {
 		return nil, fmt.Errorf("failed to login")
 	}
-	return &Client{
-		httpClient: client,
-	}, nil
+
+	return &Client{httpClient: client}, nil
 }
 
 type User struct {
@@ -67,6 +68,7 @@ type User struct {
 }
 
 func (i *Client) ListUsers() (map[string]User, error) {
+	// Get csrf tokens from the staff page to load users
 	resp, err := i.httpClient.Get("https://my.izettle.com/settings/staff")
 	if err != nil {
 		return nil, err
@@ -81,36 +83,38 @@ func (i *Client) ListUsers() (map[string]User, error) {
 	n = doc.Find("[name=csrf-token]").First()
 	token, _ := n.Attr("content")
 
+	// Get all active users
 	staffURL := fmt.Sprintf("https://my.izettle.com/settings/staff?filter=accepted&%s=%s", param, url.QueryEscape(token))
 	req, _ := http.NewRequest("GET", staffURL, nil)
+	// These headers are required, don't ask me why...
 	req.Header.Add("X-CSRF-Token", token)
 	req.Header.Add("X-Requested-With", "XMLHttpRequest")
 	resp, err = i.httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
-
 	doc, err = goquery.NewDocumentFromReader(resp.Body)
 	_ = resp.Body.Close()
 	if err != nil {
 		return nil, err
 	}
 	users := make(map[string]User)
-	nodes := doc.Find(".user")
-	if len(nodes.Nodes) == 0 {
-		return nil, fmt.Errorf("could not find any staff")
+	userNodes := doc.Find(".user")
+	if len(userNodes.Nodes) == 0 {
+		return nil, fmt.Errorf("failed to list users")
 	}
-	for _, n := range nodes.Nodes {
+	for _, n := range userNodes.Nodes {
+		// The name is formatted "KOMITEE .", we ae only interested in the "first name"
 		nameValue := doc.FindNodes(n).Find(".name").Nodes[0].FirstChild.Data
-		idValue, _ := doc.FindNodes(n).Find(".button").Attr("data-overlay-from")
 		name := strings.ToLower(strings.Split(nameValue, " ")[0])
+		// The id is formatted "#user-123", we are only interested in the numbers
+		idValue, _ := doc.FindNodes(n).Find(".button").Attr("data-overlay-from")
 		id := strings.Split(idValue, "-")[1]
 		users[name] = User{
 			ID:   id,
 			Name: name,
 		}
 	}
-
 	return users, nil
 }
 
@@ -121,6 +125,8 @@ type Report struct {
 }
 
 func (i *Client) ListReports(user User) ([]Report, error) {
+	// Get the report summary for a user, this includes all reports which are available
+	// grouped by month.
 	resp, err := i.httpClient.Get("https://my.izettle.com/reports/summary?user=" + user.ID)
 	if err != nil {
 		return nil, err
@@ -130,11 +136,16 @@ func (i *Client) ListReports(user User) ([]Report, error) {
 	if err != nil {
 		return nil, err
 	}
+	// Based on the JSON structure we are planning on serializing
+	//          "daily"."2019-01".[*].{aggregateStart,...}
+	// We leave the leaves as raw messages since they can both contain
+	// strings and nested objects.
 	var data map[string]map[string][]map[string]*json.RawMessage
 	err = json.Unmarshal(body, &data)
 	if err != nil {
 		return nil, err
 	}
+	// Time to flatten the data
 	var reports []Report
 	months := data["daily"]
 	for _, month := range months {
@@ -148,6 +159,11 @@ func (i *Client) ListReports(user User) ([]Report, error) {
 			if !ok {
 				return nil, fmt.Errorf("report summary does not have a timestamp")
 			}
+			// Since we do not run json.UnMarshal(...) the string will begin with '"',
+			// create a new slice ignoring the initial character.
+			// This timestamp is in the form 2019-09-21T01:56:09.462+0000.
+			// and we only care about the year month and day. Therefor we also
+			// cut the string at T. There are nicer ways of doing this but this works
 			end := strings.Split(string((*endTime)[1:]), "T")[0]
 			reports = append(reports, Report{
 				User: user,
@@ -156,30 +172,28 @@ func (i *Client) ListReports(user User) ([]Report, error) {
 			})
 		}
 	}
-
 	return reports, nil
 }
 
-func (i *Client) getAuthToken() (string, error) {
+// getAuthorization returns an authorization token from the cookie jar
+func (i *Client) getAuthorization() (string, error) {
 	host, _ := url.Parse("https://izettle.com")
 	cookies := i.httpClient.Jar.Cookies(host)
 	for _, c := range cookies {
 		if c.Name == "_izsessionat" {
-			return c.Value, nil
+			return "Bearer " + c.Value, nil
 		}
 	}
-	return "", fmt.Errorf("could not find auth token in cookies")
+	return "", fmt.Errorf("could not find authentication token in the cookie jar")
 }
 
-func (i *Client) GenerateReport(report Report) (io.Reader, error) {
-	token, err := i.getAuthToken()
+func (i *Client) authorizedGet(method, url string) ([]byte, error) {
+	authorization, err := i.getAuthorization()
 	if err != nil {
 		return nil, err
 	}
-
-	generateURL := fmt.Sprintf("https://reports.izettle.com/report/purchases/generate?fromDate=%s&toDate=%s&reportType=PDF&subAccountUserId=%s", report.From, report.To, report.User.ID)
-	req, _ := http.NewRequest("POST", generateURL, nil)
-	req.Header.Add("authorization", "Bearer "+token)
+	req, _ := http.NewRequest(method, url, nil)
+	req.Header.Add("authorization", authorization)
 	resp, err := i.httpClient.Do(req)
 	if err != nil {
 		return nil, err
@@ -189,48 +203,52 @@ func (i *Client) GenerateReport(report Report) (io.Reader, error) {
 	if err != nil {
 		return nil, err
 	}
+	return body, nil
+}
+
+func (i *Client) authorizedGetJSON(method, url string, out interface{}) error {
+	body, err := i.authorizedGet(method, url)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(body, out)
+}
+
+func (i *Client) ReportToPDF(report Report) (io.Reader, error) {
+	// Trigger report generation
+	reportType := "PDF"
+	generateURL := fmt.Sprintf("https://reports.izettle.com/report/purchases/generate?fromDate=%s&toDate=%s&reportType=%s&subAccountUserId=%s", report.From, report.To, reportType, report.User.ID)
 	var generateResult map[string]string
-	err = json.Unmarshal(body, &generateResult)
+	err := i.authorizedGetJSON("POST", generateURL, &generateURL)
 	if err != nil {
 		return nil, err
 	}
 
+	// Wait for the report to be generated.
 	maxTries := 10
 	tries := 0
 	for tries < maxTries {
 		tries++
 		statusURL := fmt.Sprintf("https://reports.izettle.com/report/purchases/%s/status", generateResult["uuid"])
-		req, _ = http.NewRequest("GET", statusURL, nil)
-		req.Header.Add("authorization", "Bearer "+token)
-		resp, err = i.httpClient.Do(req)
-		if err != nil {
-			return nil, err
-		}
-		body, err = ioutil.ReadAll(resp.Body)
-		_ = resp.Body.Close()
 		var status map[string]string
-		err = json.Unmarshal(body, &status)
+		err := i.authorizedGetJSON("GET", statusURL, &status)
 		if err != nil {
 			return nil, err
 		}
 		if status["status"] == "PROCESSED" {
 			break
 		}
-		time.Sleep(3)
+		time.Sleep(time.Duration(tries) * time.Second)
 	}
 	if tries == maxTries {
-		return nil, fmt.Errorf("")
+		return nil, fmt.Errorf("the report ")
 	}
 
+	// Fetch the report
 	reportURL := fmt.Sprintf("https://reports.izettle.com/report/purchases/%s", generateResult["uuid"])
-	req, _ = http.NewRequest("GET", reportURL, nil)
-	req.Header.Add("authorization", "Bearer "+token)
-	resp, err = i.httpClient.Do(req)
+	body, err := i.authorizedGet("GET", reportURL)
 	if err != nil {
 		return nil, err
 	}
-	body, err = ioutil.ReadAll(resp.Body)
-	_ = resp.Body.Close()
-
 	return bytes.NewReader(body), nil
 }
